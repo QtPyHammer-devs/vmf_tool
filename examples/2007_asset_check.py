61031db8f989774c4d155f3f0ef873b758ee13ac
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2018-09-14 23:03:55 +1000

    partial bugfixes

--- examples/2007_asset_check.py
+++ examples/2007_asset_check.py
@@ -1,4 +1,3 @@
-# TODO: remove case sensitivity from list checks
 import copy
 import sys
 import vector
@@ -18,34 +17,42 @@ def check_materials(vmf, allowed_materials):
             if isinstance(entity.solids[0], vmf_tool.namespace):
                 all_brushes += entity.solids
     for brush in all_brushes:
-        try:
-            for side in brush.sides:
-                if not side.material in allowed_materials:
-                    face_triangle = [[float(i) for i in xyz.split()] for xyz in side.plane[1:-1].split(') (')]
-                    face_center = sum([vector.vec3(*v) for v in face_triangle], vector.vec3()) / 3
-                    print(f'Brush #{brush.id} Face @ {face_center:.2f} uses {side.material}')
-                    # View > Go To Brush Number... (Ctrl+Shift+G) in hammer
-        except Exception as exc:
-            print(brush)
-            raise exc
+        for side in brush.sides:
+            if side.material.upper() not in allowed_materials:
+                face_triangle = [[float(i) for i in xyz.split()] for xyz in side.plane[1:-1].split(') (')]
+                face_center = sum([vector.vec3(*v) for v in face_triangle], vector.vec3()) / 3
+                yield f'Brush #{brush.id} Face @ {face_center:.2f} uses {side.material}'
+                # View > Go To Brush Number... (Ctrl+Shift+G) in hammer
     
 
-def check_props(vmf, allowed_props):
+def check_props(vmf, allowed_props): # BUGGED?
     if not hasattr(vmf, 'entities') and hasattr(vmf, 'entity'): # one entity
         vmf.entities = [vmf.entity]
         del vmf.entity
     for entity in vmf.entities:
         if 'prop' in entity.classname:
-            if entity.model not in allowed_props:
-                short_propname = entity.model.lstrip('models/')
-                print(f'{entity.classname} @ {entity.origin} ({short_propname})')
+            if entity.model.upper() not in allowed_props:
+                short_propname = entity.model.lstrip('MODELS/')
+                yield f'{entity.classname} @ {entity.origin} ({short_propname})'
                 # View > Go To Coordinates... in hammer
 
 
 if __name__ == "__main__":
     prop_list = open('2007_props.csv').read().split(',')
     material_list = open('2007_materials.csv').read().split(',') # should include tool textures
+##    sys.argv.append('E:/Steam/SteamApps/common/sourcesdk_content/tf/mapsrc/sdk_cp_gravelpit.vmf')
     for vmf_file in sys.argv[1:]:
+        print(f'Checking {vmf_file}...')
         vmf = vmf_tool.namespace_from(open(vmf_file))
-        check_materials(vmf, material_list)
-        check_props(vmf, prop_list)
+        log = open(vmf_file[:-4] + '_asset_log.txt', 'w')
+        print('### MATERIALS ###')
+        log.write('### MATERIALS ###\n')
+        for warning in check_materials(vmf, material_list):
+            print(warning)
+            log.write(warning + '\n')
+        print('### PROPS ###')
+        log.write('### PROPS ###\n')
+        for warning in check_props(vmf, prop_list):
+            print(warning)
+            log.write(warning + '\n')
+        log.close()
