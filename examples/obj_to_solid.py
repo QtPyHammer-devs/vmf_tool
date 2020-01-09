a1e7bd368d2e97f45f29f4f09eb73df27d3039bc
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2020-01-09 14:32:49 +1000

    updated obj to solid example script

--- examples/obj_to_solid.py
+++ examples/obj_to_solid.py
@@ -9,50 +9,51 @@ def obj_solids(filepath): # split by object and group
     brush_id = 0
     solids = []
     solids.append(brush.copy())
-    solids[-1]['id'] = brush_id
+    solids[-1]["id"] = brush_id
     brush_id += 1
     for line in file.readlines():
-        line = line.rstrip('\n')
-        if line.startswith('v'):
-            v.append([float(f) for f in line.split(' ')[1:]])
-        elif line.startswith('f'):
-            line = line.replace('\\', '/').split(' ')[1:]
+        line = line.rstrip("\n")
+        if line.startswith("v"):
+            v.append([float(f) for f in line.split(" ")[1:]])
+        elif line.startswith("f"):
+            line = line.replace("\\", "/").split(" ")[1:]
             plane = []
             for point in line[:3]:
-                vertex = v[int(point.split('/')[0]) - 1]
-                plane.append(' '.join([str(x) for x in vertex]))
+                vertex = v[int(point.split("/")[0]) - 1]
+                plane.append(" ".join([str(x) for x in vertex]))
             plane = reversed(plane)
             side_id += 1
-            solids[-1]['sides'].append({
-                'id': side_id,
-                'plane': '(' + ') ('.join(plane) + ')',
-                'material': 'TOOLS/TOOLSNODRAW',
-                'uaxis': '[0 -1 0 0] 0.25',
-		'vaxis': '[0 0 -1 0] 0.25',
-		'rotation': '0',
-		'lightmapscale': '16',
-		'smoothing_groups': '0'})
-        elif line.startswith('o'):
+            solids[-1]["sides"].append({
+                "id": side_id,
+                "plane": "(" + ") (".join(plane) + ")",
+                "material": "TOOLS/TOOLSNODRAW",
+                "uaxis": "[0 -1 0 0] 0.25",
+		"vaxis": "[0 0 -1 0] 0.25",
+		"rotation": "0",
+		"lightmapscale": "16",
+		"smoothing_groups": "0"})
+        elif line.startswith("o"):
             solids.append(brush.copy())
-            solids[-1]['id'] = brush_id
+            solids[-1]["id"] = brush_id
             brush_id += 1
-    if len(solids[0]['sides']) == 0:
+    if len(solids[0]["sides"]) == 0:
         solids = solids[1:]
     file.close()
     return solids
 
 if __name__ == "__main__":
     import sys
-    sys.path.insert(0, '../')
+    sys.path.insert(0, "../")
     import vmf_tool
-    with open('../mapsrc/blank.vmf') as base_file:
+    with open("../mapsrc/blank.vmf") as base_file:
         base = vmf_tool.parse_lines(base_file.readlines())
+        
     for filepath in sys.argv[1:]:
-        base.dict['world']['solids'] = obj_solids(filepath)
-        with open(f'{filepath}.vmf', 'w') as out_file:
-            export(base, out_file)
+        base.world.solids = obj_solids(filepath)
+        with open(filepath + ".vmf", "w") as out_file:
+            vmf_tool.export(base, out_file)
 
-    ##TEST    
-    # hemisphere = obj_solids('hemisphere.obj')
-    # base.dict['world']['solids'] = hemisphere
-    # base.export(open('hemisphere.vmf', 'w'))
+    # TEST
+    base.world.solids = obj_solids("objs/hemisphere.obj")
+    with open("vmfs/hemisphere.vmf", "w") as out_file:
+        vmf_tool.export(base, out_file)
