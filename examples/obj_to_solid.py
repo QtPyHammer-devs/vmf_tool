bb6695b7debc4de618f011d29e30f819d6ba83cf
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-03-05 11:22:38 +1000

    fixed a bug where multi-objects vmf files would become bloated and crash hammer upon loading

--- examples/obj_to_solid.py
+++ examples/obj_to_solid.py
@@ -1,20 +1,30 @@
-def obj_solids(filepath): # split by object and group
-    """turns faces into sides of a .vmf solid"""
+nodraw = "TOOLS/TOOLSNODRAW"
+skip = "TOOLS/TOOLSSKIP"
+wire = "EDITOR/WIREFRAME"
+
+def obj_solids(filepath, material=nodraw):
+    """turns .obj mesh objects into .vmf solids"""
     file = open(filepath)
     v  = []
     _id = 1
     brush = {"sides": [], "editor":
                 {"color": "255 0 255", "visgroupshown": "1",
                  "visgroupautoshown": "1"}}
+    current_brush = None
     solids = []
-    solids.append(brush.copy())
-    solids[-1]["id"] = _id
     _id += 1
     for line in file.readlines():
         line = line.rstrip("\n")
-        if line.startswith("v"):
+        if line.startswith("o"): # new object, new brush
+            if current_brush != None:
+                solids.append(current_brush)\
+            current_brush = brush.copy()
+            current_brush["id"] = _id
+            current_brush["sides"] = []
+            _id += 1
+        elif line.startswith("v"): # vertex
             v.append([float(f) for f in line.split(" ")[1:]])
-        elif line.startswith("f"):
+        elif line.startswith("f"): # face (side)
             line = line.replace("\\", "/").split(" ")[1:]
             plane = []
             for point in line[:3]:
@@ -22,21 +32,15 @@ def obj_solids(filepath): # split by object and group
                 plane.append(" ".join([str(x) for x in vertex]))
             plane = reversed(plane)
             _id += 1
-            solids[-1]["sides"].append({
+            current_brush["sides"].append({
                 "id": _id,
                 "plane": "(" + ") (".join(plane) + ")",
-                "material": "TOOLS/TOOLSNODRAW",
+                "material": material,
                 "uaxis": "[0 -1 0 0] 0.25",
 		"vaxis": "[0 0 -1 0] 0.25",
 		"rotation": "0",
 		"lightmapscale": "16",
 		"smoothing_groups": "0"})
-        elif line.startswith("o"): # new object, new brush
-            solids.append(brush.copy())
-            solids[-1]["id"] = _id
-            _id += 1
-    if len(solids[0]["sides"]) == 0:
-        solids = solids[1:]
     file.close()
     return solids
 
@@ -46,7 +50,7 @@ if __name__ == "__main__":
     import vmf_tool
     with open("../mapsrc/blank.vmf") as base_file:
         base = vmf_tool.parse_lines(base_file.readlines())
-        
+
     for filepath in sys.argv[1:]:
         base.world.solids = obj_solids(filepath)
         with open(filepath + ".vmf", "w") as out_file:
