d68ca5f08b6ea091a51524c3298d46540a8fbac0
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-09-16 14:40:27 +1000

    cleaning up some linter warnings

--- vmf_tool/parser.py
+++ vmf_tool/parser.py
@@ -1,11 +1,10 @@
 import io
-import re
 
 
 def parse(string_or_file):
     """String or .vmf file to nested namespace"""
     if not isinstance(string_or_file, (str, io.TextIOWrapper, io.StringIO)):
-        raise RuntimeError(f"Object given to parse is not a string or a file")
+        raise RuntimeError(f"{string_or_file} is neither a string nor a file")
     if isinstance(string_or_file, str):
         file = io.StringIO(string_or_file) # make string file-like
     else:
@@ -15,63 +14,64 @@ def parse(string_or_file):
     previous_line = ""
     for line_number, line in enumerate(file.readlines()):
         try:
-            new_namespace = namespace({'_line': line_number})
+            new_namespace = namespace({"_line": line_number})
             current_target = current_scope.get_from(nest)
             line = line.strip()  # cleanup spacing
-            if line == "" or line.startswith("//"): # ignore blank / comments
+            if line == "" or line.startswith("//"):  # ignore blank / comments
                 continue
-            elif line =="{": # START declaration
+            elif line == "{": # START declaration
                 current_keys = current_target.__dict__.keys()
                 plural = pluralise(previous_line)
-                if previous_line in current_keys: # NEW plural
+                if previous_line in current_keys:  # NEW plural
                     current_target[plural] = [current_target[previous_line]] # create plural from old singular
-                    current_target.__dict__.pop(previous_line) # delete singular
-                    current_target[plural].append(new_namespace) # second entry
+                    current_target.__dict__.pop(previous_line)  # delete singular
+                    current_target[plural].append(new_namespace)  # second entry
                     current_scope.add(plural)
-                    current_scope.add(1) # point at new_namespace
-                elif plural in current_keys: # APPEND plural
-                    current_scope.add(plural) # point at plural
+                    current_scope.add(1)  # point at new_namespace
+                elif plural in current_keys:  # APPEND plural
+                    current_scope.add(plural)  # point at plural
                     current_scope.get_from(nest).append(new_namespace)
-                    current_scope.add(len(current_scope.get_from(nest)) - 1) # current index in plural
-                else: # NEW singular
+                    current_scope.add(len(current_scope.get_from(nest)) - 1)  # current index in plural
+                else:  # NEW singular
                     current_scope.add(previous_line)
                     current_scope.set_in(nest, new_namespace)
-            elif line == '}': # END declaration
+            elif line == "}":  # END declaration
                 current_scope.retreat()
-            elif '" "' in line: # KEY VALUE
+            elif '" "' in line:  # "KEY" "VALUE"
                 key, value = line.split('" "')
                 key = key.lstrip('"')
                 value = value.rstrip('"')
                 current_target[key] = value
-            elif line.count(' ') == 1:
+            elif line.count(" ") == 1: # KEY VALUE
                 key, value = line.split()
                 current_target[key] = value
-            previous_line = line.strip('"')
+            previous_line = line.strip('"') # for naming scope, if line == "{"
         except Exception as exc:
             print("error on line {0:04d}:\n{1}\n{2}".format(line_number, previous_line, line))
             raise exc
     return nest
 
-def text_from(_dict, tab_depth=0): # rethink & refactor
+
+def text_from(_dict, tab_depth=0):  # rethink & refactor
     """Nested namespaces / dicts --> text resembling a .vmf"""
     out = []
     tabs = "\t" * tab_depth
     for key, value in _dict.items():
         if key == "_line":
             continue
-        elif isinstance(value, str): # key-value pair
+        elif isinstance(value, str):  # key-value pair
             out.append(f"""{tabs}"{key}" "{value}"\n""")
             continue
-        elif isinstance(value, (dict, namespace)): # another nest
+        elif isinstance(value, (dict, namespace)):  # another nest
             value = (value,)
-        elif isinstance(value, (list, tuple)): # collection of plurals
+        elif isinstance(value, (list, tuple)):  # collection of plurals
             key = singularise(key)
         else:
             raise RuntimeError(f"Found a non-string: {value}")
-        for item in value: # go a layer deeper
+        for item in value:  # go a layer deeper
             out.append(f"""{tabs}{key}\n{tabs}""" + "{\n")
             out.append(text_from(item, tab_depth + 1))
-    if tab_depth > 0: # close the plural index / namespace
+    if tab_depth > 0:  # close the plural index / namespace
         out.append("\t" * (tab_depth - 1) + "}\n")
     return "".join(out)
 
@@ -117,11 +117,11 @@ class scope:
         if isinstance(popped, int):
             self.tiers.pop(-1)
 
-    def remove_from(self, nest): # used for changing singulars into plurals
+    def remove_from(self, nest):  # used for changing singulars into plurals
         """Delete the item stored in nest which this scope points at"""
         target = nest
         for i, tier in enumerate(self.tiers):
-            if i == len(self.tiers) - 1: # must set from tier above
+            if i == len(self.tiers) - 1:  # must set from tier above
                 target.pop(tier)
             else:
                 target = target[tier]
@@ -134,13 +134,13 @@ class scope:
         """Sets the item stored in nest which this scope points at to value"""
         target = nest
         for i, tier in enumerate(self.tiers):
-            if i == len(self.tiers) - 1: # must set from tier above
-                target[tier] = value # could be creating this value
+            if i == len(self.tiers) - 1:  # must set from tier above
+                target[tier] = value  # could be creating this value
             else:
                 target = target[tier]
 
 
-class namespace: # this name doesn't tell me what this thing does
+class namespace:  # this name doesn't tell me what this thing does
     def __init__(self, _dict=dict()):
         for key, value in _dict.items() if isinstance(_dict, dict) else _dict.__dict__.items():
             if isinstance(value, dict):
@@ -177,24 +177,24 @@ class namespace: # this name doesn't tell me what this thing does
 
 
 def pluralise(word):
-    if word.endswith('f'): # self -> selves
+    if word.endswith('f'):  # self -> selves
         return word[:-1] + 'ves'
-    elif word.endswith('y'): # body -> bodies
+    elif word.endswith('y'):  # body -> bodies
         return word[:-1] + 'ies'
-    elif word.endswith('ex'): # vertex -> vertices
+    elif word.endswith('ex'):  # vertex -> vertices
         return word[:-2] + 'ices'
-    else: # side -> sides
+    else:  # side -> sides
         return word + 's'
 
 def singularise(word):
-    if word.endswith('ves'): # self <- selves
+    if word.endswith('ves'):  # self <- selves
         return word[:-3] + 'f'
-    elif word.endswith('ies'): # body <- bodies
+    elif word.endswith('ies'):  # body <- bodies
         return word[:-3] + 'y'
-    elif word.endswith('ices'): # vertex <- vertices
+    elif word.endswith('ices'):  # vertex <- vertices
         return word[:-4] + 'ex'
     # in the face of ambiguity, refuse the temptation to guess
-    elif word.endswith('s'): # side <- sides
+    elif word.endswith('s'):  # side <- sides
         return word[:-1]
     else:
-        return word # assume word is already singular
+        return word  # assume word is already singular
