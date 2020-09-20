4bfb6723a2298cf3f212723458f9dc60a2c07cfb
Jared Ketterer <36507175+snake-biscuits@users.noreply.github.com>
2020-09-20 21:27:07 +1000

    need python 3.7+ for proper type hints; dropped python 3.6 support

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setuptools.setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8"
     ],
--- vmf_tool/__init__.py
+++ vmf_tool/__init__.py
@@ -1,6 +1,7 @@
 """A library for interpreting & editing .vmf files"""
+
+__all__ = ["brushes", "parser", "Vmf"]
+
 from . import brushes
 from . import parser
 from .vmf import Vmf
-
-__all__ = ["brushes", "parser", "Vmf"]
