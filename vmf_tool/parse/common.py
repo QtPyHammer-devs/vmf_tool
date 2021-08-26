db1f7947ab2044d4f1691da03d2bb852e7885dfc
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2021-08-26 14:58:49 +0100

    refactor 75% complete

--- vmf_tool/parse/common.py
+++ vmf_tool/parse/common.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import re
-from typing import Any, ItemsView, Iterable, List, Mapping, Union
+from typing import Any, ItemsView, Iterable, List, Mapping, Tuple
 
 
 # Glossary
@@ -81,21 +81,26 @@ class Namespace:
     """Maps objects like a dictionary, all keys are strings.
     Values can be accessed as class attributes.
     If a key is not a valid attribute name, if can be used like a dictionary key."""
-    def __init__(self, **presets: Mapping[str, Union[Namespace, List, str]]):
-        # Namespace(key=value, key2=value2)
-        for key, value in presets.items():
-            if isinstance(value, dict):
-                self[key] = Namespace(value)
-            elif isinstance(value, list):
-                self[key] = [Namespace(i) for i in value]
-            else:
+    def __init__(self, *args: List[Tuple[str, Any]], **presets: Mapping[str, Any]):
+        # args = Namespace([("key", value), ("key", "value2"), ("key2", value)])
+        # kwargs = Namespace(key=value, key2=value2)
+        for kvp_list in (*args, presets.items()):
+            for key, value in kvp_list:
                 self[key] = value
 
-    def __setitem__(self, index: Any, value: Any):
-        setattr(self, str(index), value)
+    # TODO: def __delitem__(self, key: str):
 
-    def __getitem__(self, index: Any) -> Any:
-        return self.__dict__[str(index)]
+    def __setitem__(self, key: str, value: Any):
+        if key in self:  # make plural
+            old_value = getattr(self, key)
+            if isinstance(old_value, list):
+                value = old_value
+            else:
+                value = [old_value, value]
+        setattr(self, key, value)
+
+    def __getitem__(self, key: str) -> Any:
+        return self.__dict__[key]
 
     def __iter__(self) -> Iterable:
         return iter(self.__dict__.keys())
@@ -104,6 +109,7 @@ class Namespace:
         return len(self.__dict__.keys())
 
     def __repr__(self) -> str:
+        # TODO: consider string size and communicate depth better
         attributes: List[str] = list()
         for attribute_name, attr in self.items():
             if not re.match("^[A-Za-z_][A-Za-z_0-9]*$", attribute_name):
@@ -120,33 +126,10 @@ class Namespace:
         else:
             self[attr] = value
 
-    def as_string(self, depth: int = 0) -> str:
-        """Namespace / dictionary --> text resembling a .vmf"""
-        out = list()
-        indent = "\t" * depth
-        for key, value in self.items():
-            if key == "_line":
-                continue  # ignore line numbers
-            # BRANCH A: Key-Value Pair
-            elif isinstance(value, str):  # key-value pair
-                out.append(f"""{indent}"{key}" "{value}"\n""")
-            # BRANCH B-1: Namespace / "Plural" of Namespaces
-            elif isinstance(value, (dict, Namespace)):  # singular
-                out.append(f"""{indent}{key}\n{indent}""" + "{\n")
-                out.append(value.as_string(depth + 1))
-            elif isinstance(value, list):
-                for child in value:
-                    if isinstance(child, str):  # key occured more that once (entity connections etc.)
-                        out.append(f"""{indent}"{key}" "{child}"\n""")
-                    elif isinstance(child, (dict, Namespace)):  # BRANCH B-2: Recurse
-                        if len([k for k in child if k != "_line"]) > 0:  # ignore The Empty Child
-                            out.append(f"""{indent}{key}\n{indent}""" + "{\n")
-                            out.append(child.as_string(depth + 1))
-            else:
-                raise RuntimeError(f"Found a non-string: {value}")
-        if depth > 0:  # close BRANCH B-2 for parent
-            out.append("\t" * (depth - 1) + "}\n")
-        return "".join(out)
+    def update(self, *args: List[Tuple[str, Any]], **kwargs: Mapping[str, Any]):
+        for kvp_list in (*args, kwargs.items()):
+            for key, value in kvp_list:
+                self[key] = value
 
     def items(self) -> ItemsView:
         """exposes self.__dict__"""
--- vmf_tool/parse/vmf.py
+++ vmf_tool/parse/vmf.py
@@ -4,35 +4,72 @@ from . import common
 
 
 # regex patterns
-named_object = r"(\w+)\s*?\{\s*?([^\{\}]*)\}\s*?"
+pattern_named_object = re.compile(r"(\w+)\s*?\{\s*?([^\{\}]*)\}\s*?")
 # ^ name, contents = match.groups()
 # NOTE: starts at the "youngest generation" (those without children)
-key_value_pair = r'(?<=")([^"]*)(?=")\s(?=")([^"]*)(?=")'
+pattern_key_value_pair = re.compile(r'(?<=")([^"]*)(?=")\s(?=")([^"]*)(?=")')
 # ^ key, value = match.groups()
-
-pattern_named_object = re.compile(named_object)
-pattern_key_value_pair = re.compile(key_value_pair)
 # TODO: warnings for `{`, `}` or `"` in key-value-pair
+pattern_all_whitespace = re.compile(r"^\s*$")
+# NOTE: ignoring comments, the last parser caught those
 
 
 def as_namespace(vmf_text: str) -> common.Namespace:
     """.vmf text -> Namespace"""
     # TODO: line number when reporting errors
-    child_tier = dict()  # last tier
-    current_tier = dict()
-    # ^ {(match.name, match.start(), match.end()): Namespace(match.contents)}
-    for match in pattern_named_object.finditer(vmf_text):
-        print(f"{match = }")
-        name, contents = match.groups()
-        print(name, "{", contents, "}", sep="\n")
-        namespace = common.Namespace({k: v for k, v in pattern_key_value_pair.findall(contents)})
-        # NOTE: dict assembling oneliner handles duplicate keys poorly
-        # TODO: identify children from last iteration and assimilate into namespace
-        current_tier[(name, match.start(), match.end())] = namespace
-        # TODO: convert block contents to namespace (recursively [via common.Scope?])
-        # TODO: out_namespace[block.group("name")] = as_namespace(block.contents)
-        vmf_text = vmf_text[:match.start()] + vmf_text[match.end():]
-        # remove self, so parents may be parsed
-        # NOTE: might have to do some match location tracking to indetify parental lineage
-        # still, could do a while loop above this to match every named_object
-    return common.Namespace({n: current_tier[(n, s, e)] for n, s, e in current_tier})
+    child_tier = dict()  # final namespace is assembled innermost object first
+    while pattern_all_whitespace.match(vmf_text) is not None:
+        # TODO: invalid data will loop forever!  catch this and located the invalid text!
+        current_tier = dict()
+        # ^ {(match.name, match.start, match.end): Namespace(match.contents)}
+        for match in pattern_named_object.finditer(vmf_text):
+            print(f"{match = }")
+            name, contents = match.groups()
+            print(name, "{", contents, "}", sep="\n")
+            namespace = common.Namespace(pattern_key_value_pair.findall(contents))
+            # TODO: ensure all key value pairs are collected
+            start, end = match.span()
+            namespace.update([child_tier[(s, e)] for s, e in child_tier if start < s < e <= end])
+            # TODO: remove children from the pool when their parents collect them
+            current_tier[(start, end)] = (name, namespace)
+            # TODO: out_namespace[block.group("name")] = as_namespace(block.contents)
+            # replace self with whitespace, so parents may be parsed
+            vmf_text[start:end] = "".join([(" "if c != "\n" else "\n") for c in vmf_text[start:end]])
+        # TODO: ensure no child is left behind
+        child_tier = current_tier.copy()
+    return common.Namespace(current_tier.values())
+
+
+def as_vmf(namespace: common.Namespace, depth: int = 0) -> str:
+    """Namespace / dict --> .vmf text"""
+    out = list()
+    indent = "\t" * depth
+    # TODO: convert non-dict/list/Namespace values to strings for writing
+    for key, value in namespace.items():
+        if key == "_line":
+            continue  # ignore line numbers
+        # BRANCH A: Key-Value Pair
+        elif isinstance(value, str):
+            out.append(f"""{indent}"{key}" "{value}"\n""")
+        # BRANCH B-1: Singular Namespace
+        elif isinstance(value, (dict, common.Namespace)):
+            out.append(f"""{indent}{key}\n{indent}""" + "{\n")
+            out.append(value.as_string(depth + 1))
+        # BRANCH B-2: Plural Namespaces (maybe)
+        elif isinstance(value, list):
+            for child in value:
+                # BRANCH C: Non-plural, duplicate key
+                if isinstance(child, str):
+                    # e.g. brush entity "solid" type key-value pair + solid Namespace(s)
+                    out.append(f"""{indent}"{key}" "{child}"\n""")
+                elif isinstance(child, (dict, common.Namespace)):
+                    # BRANCH D: Confirmed Plural: Recurse
+                    if len([k for k in child if k != "_line"]) > 0:  # ignoring line numbers
+                        out.append(f"""{indent}{key}\n{indent}""" + "{\n")
+                        out.append(child.as_string(depth + 1))
+        else:
+            # all elements must be strings before converting!
+            raise RuntimeError(f"Found a non-string: {value}")
+    if depth > 0:  # close BRANCH B-2 for parent
+        out.append("\t" * (depth - 1) + "}\n")
+    return "".join(out)
